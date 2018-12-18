5e436b77a5f2f40f4aa31ee727470a1b34e13f1e
Matteo Sorrentino <Ciro & Luisa@DESKTOP-1GLGU9U>
2018-12-18 15:52:50 +0100

    Creazione di codice per conversione di un numero decimale a binario

---
+++ Solution/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+
+            Console.WriteLine("inserisci un numero da convertire a base 2");
+
+            value = Convert.ToInt32(Console.ReadLine());
+
+            string binary = Convert.ToString(value, 2);
+
+            Console.WriteLine($"il numero {value} convertito in base 2 diventa {binary}");
+
+            Console.ReadLine();
+        }
+    }
+}
